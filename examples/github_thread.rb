98c924edc1a213d916d49f1566ffddffd68a0f41
Jan Graichen <jg@altimos.de>
2014-05-11 17:03:57 +0200

    Add example

---
+++ examples/github_thread.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+$LOAD_PATH << File.expand_path('../../../obligation/lib', __FILE__)
+
+require 'restify'
+require 'pry'
+require 'pry-nav'
+
+gh   = Restify.new('https://api.github.com').value
+repo = gh.rel(:repository).get(owner: 'jgraichen', repo: 'restify').value
+cmt  = repo.rel(:commits).get.value.first
+
+puts "Last commit: #{cmt[:sha]}"
+puts "By #{cmt[:commit][:author][:name]} <#{cmt[:commit][:author][:email]}>"
+puts "#{cmt[:commit][:message]}"
