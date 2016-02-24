060edd8dddbb488c4775b17d2e093952c01f83a4
Jan Graichen <jg@altimos.de>
2016-02-24 10:48:20 +0100

    Release 0.4.0

--- lib/restify/version.rb
+++ lib/restify/version.rb
@@ -3,7 +3,7 @@ module Restify
     MAJOR = 0
     MINOR = 4
     PATCH = 0
-    STAGE = 'b1'.freeze
+    STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
 
     def self.to_s
