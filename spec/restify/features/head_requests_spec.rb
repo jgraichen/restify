5d0d9f689baa25059aa94094f0c03d772c7c898f
Jan Graichen <jgraichen@altimos.de>
2018-08-22 16:08:36 +0200

    Fix linter warnings

--- spec/restify/features/head_requests_spec.rb
+++ spec/restify/features/head_requests_spec.rb
@@ -7,21 +7,21 @@ describe Restify do
     stub_request(:head, 'http://localhost/base')
       .with(query: hash_including({}))
       .to_return do
-        <<-EOF.gsub(/^ {10}/, '')
-          HTTP/1.1 200 OK
-          Content-Length: 333
-          Transfer-Encoding: chunked
-          Link: <http://localhost/other>; rel="neat"
-        EOF
+      <<-RESPONSE.gsub(/^ {8}/, '')
+        HTTP/1.1 200 OK
+        Content-Length: 333
+        Transfer-Encoding: chunked
+        Link: <http://localhost/other>; rel="neat"
+      RESPONSE
     end
   end
 
   describe 'HEAD requests' do
     subject { Restify.new('http://localhost/base').head(params).value! }
-    let(:params) {  {} }
+    let(:params) { {} }
 
     it 'returns a resource with access to headers' do
-      expect(subject.response.headers).to include({'CONTENT_LENGTH' => '333'})
+      expect(subject.response.headers).to include('CONTENT_LENGTH' => '333')
     end
 
     it 'parses Link headers into relations' do
