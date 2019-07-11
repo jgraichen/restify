fdd15a9b9ad6364b7e0eebce14ee4cfdf095ab3b
Jan Graichen <jgraichen@altimos.de>
2019-07-11 19:27:53 +0200

    Release v1.11

--- lib/restify/version.rb
+++ lib/restify/version.rb
@@ -3,7 +3,7 @@
 module Restify
   module VERSION
     MAJOR = 1
-    MINOR = 10
+    MINOR = 11
     PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
