77da5f3e3dc612ecab52fc5b89850f047be2057d
Franz Liedke <franz@develophp.org>
2021-05-06 23:26:31 +0200

    TooManyRequests: Parse Retry-After header

--- spec/restify/features/response_errors_spec.rb
+++ spec/restify/features/response_errors_spec.rb
@@ -4,10 +4,12 @@ require 'spec_helper'
 
 describe Restify do
   let!(:request_stub) do
-    stub_request(:get, 'http://stubserver/base').to_return(status: http_status)
+    stub_request(:get, 'http://stubserver/base')
+      .to_return(status: http_status, headers: headers)
   end
 
   let(:http_status) { '200 OK' }
+  let(:headers) { {} }
 
   describe 'Error handling' do
     subject(:request) { Restify.new('http://localhost:9292/base').get.value! }
@@ -57,6 +59,52 @@ describe Restify do
 
       it 'throws a TooManyRequests exception' do
         expect { request }.to raise_error Restify::TooManyRequests
+      end
+
+      describe 'the exception' do
+        subject(:exception) do
+          exception = nil
+          begin
+            request
+          rescue Restify::TooManyRequests => e
+            exception = e
+          end
+          exception
+        end
+
+        context 'by default' do
+          it 'does not know when to retry again' do
+            expect(exception.retry_after).to be_nil
+          end
+        end
+
+        context 'with Retry-After header containing seconds' do
+          let(:headers) { {'Retry-After' => '120'} }
+
+          it 'determines the date correctly' do
+            now = DateTime.now
+            lower = now + Rational(119, 86_400)
+            upper = now + Rational(121, 86_400)
+
+            expect(exception.retry_after).to be_between(lower, upper)
+          end
+        end
+
+        context 'with Retry-After header containing HTTP date' do
+          let(:headers) { {'Retry-After' => 'Sun, 13 Mar 2033 13:03:33 GMT'} }
+
+          it 'parses the date correctly' do
+            expect(exception.retry_after.to_s).to eq '2033-03-13T13:03:33+00:00'
+          end
+        end
+
+        context 'with Retry-After header containing invalid date string' do
+          let(:headers) { {'Retry-After' => 'tomorrow 12:00:00'} }
+
+          it 'does not know when to retry again' do
+            expect(exception.retry_after).to be_nil
+          end
+        end
       end
     end
 
