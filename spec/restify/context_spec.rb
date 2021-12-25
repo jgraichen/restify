9cf1d9302a6d9e43e6832850dd49c8aff6a51db4
Jan Graichen <jgraichen@altimos.de>
2021-12-25 23:31:27 +0100

    fix: conditional serialize test due to YAML differences
    Ruby 3.1 defaults YAML.load to .safe_load

--- spec/restify/context_spec.rb
+++ spec/restify/context_spec.rb
@@ -52,7 +52,12 @@ describe Restify::Context do
       subject { load }
 
       let(:dump) { YAML.dump(context) }
-      let(:load) { YAML.load(dump, permitted_classes: [::Restify::Context, ::Symbol]) } # rubocop:disable Security/YAMLLoad
+
+      if RUBY_VERSION >= '3.1'
+        let(:load) { YAML.safe_load(dump, permitted_classes: [::Restify::Context, ::Symbol]) }
+      else
+        let(:load) { YAML.load(dump) } # rubocop:disable Security/YAMLLoad
+      end
 
       include_examples 'serialization'
     end
